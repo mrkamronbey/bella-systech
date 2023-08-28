2f31976fff8c13e6c91dc6050b5700b5373e3387
SarvinozSupersite <Supersite.office@gmail.com>
2023-08-28 11:19:10 +0500

    go push

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+echo "Switching to branch main"
+git checkout main
+
+echo "Building app..."
+npm run build
+
+echo "Deploying files to server..."
+scp -r build/* root@138.197.16.184:/var/www/bella/
+
+echo "Done!"
+
+
