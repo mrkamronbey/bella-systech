e204cfdade62a3b81a477ba9fb98e46344d8e19f
SarvinozSupersite <Supersite.office@gmail.com>
2023-08-29 13:12:39 +0500

    go push

--- src/components/product/premium_product/index.jsx
+++ src/components/product/premium_product/index.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import Reveal from "../../../utils/reveal/reveal";
 import styles from "./style.module.css";
-const PremiumProduct = () => {
+const PremiumProduct = ({dataproductfilter}) => {
   const { t } = useTranslation();
-  const data = [1, 2, 3, 4];
+  function LanguValue() {
+    return window.localStorage.getItem("i18nextLng");
+  }
+
   return (
     <div className={styles.box}>
     <div className={styles.Container}>
       <h2>{t("Product.3")}</h2>
         <ul>
-          {data.map((elem, index) => (
-              <li>
-                Высокая скорость - Быстрая эпиляция за счет высокой частоты
-                подачи импульсов — до 10 Гц, насадкой диаметром до 25 мм.
-                Повышается объем потенциальной прибыли
+          {dataproductfilter.map((elem, index) => (
+              <li key={index}>
+                {LanguValue() == "ru" ? elem.product_benefits : LanguValue() == "uz" ? elem.product_benefits_uz :LanguValue() == "en" ? elem.product_benefits_en :null}
               </li>
           ))}
         </ul>
